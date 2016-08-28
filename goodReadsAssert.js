dc2549cdabbb9a5315268c52c9ef208e191598b9
derek_robertson <dwilliamrobertson@gmail.com>
2016-08-28 13:02:09 -0700

    added a few assert tests and for loops for specific book identification

--- goodReadsAssert.js
+++ goodReadsAssert.js
@@ -1,5 +1,6 @@
+// create my test suite
 casper.test.begin('test login for good reads and user experience', 4, function suite(test){
-
+  //set up messages that will print to the console
   casper.on("remote.message", function(msg){
     this.echo("remote.msg: " + msg, 'INFO');
   });
@@ -7,7 +8,8 @@ casper.test.begin('test login for good reads and user experience', 4, function s
   casper.on("page.error", function(pageErr){
     this.echo("page.err: " + JSON.stringify(pageErr), 'ERROR');
   });
-
+  // open goodreads.com check to see if the login exists
+  // fill out the login with email and password and simulate a click event to submit
   casper.start("https://goodreads.com", function(){
     test.assertExists('form[name="sign_in"]', 'login form is found.');
 
@@ -17,29 +19,31 @@ casper.test.begin('test login for good reads and user experience', 4, function s
         document.querySelector('input[value="Sign in"]').click();
     });
   });
-
+  // take a screen shot to show I have logged in
   casper.then(function(){
     this.wait(3000, function(){
       console.log("Make a screenshot and save it as AfterLogin.png");
       this.capture('AfterLogin.png');
     });
   });
-
+  // check to see if the search form exists
+  // fill out the search form and submit
   casper.then(function(){
-
     test.assertExists('form[action="/search"]', 'search form found');
     this.fill('form[action="/search"]', {
       q: "Fight Club"
     }, true);
   });
-
+  // take a screenshot to show I have searched what I want
   casper.then(function(){
     this.wait(3000, function(){
       console.log("Make a screenshot and save it as AfterSearch.png");
       this.capture('AfterSearch.png');
     });
   });
-
+  // query all books on the page and loop through them looking for the first
+  // instance of the book title matching my search term and simulate a click
+  // setting the book to currently reading
   casper.then(function(){
     this.evaluate(function(){
       var tempArray = document.querySelectorAll('tr[itemtype="http://schema.org/Book"]');
@@ -55,26 +59,28 @@ casper.test.begin('test login for good reads and user experience', 4, function s
       }
     });
   });
-
+  // test to make sure the book has been set to currently reading
   casper.then(function(){
     this.wait(3000, function(){
       test.assertExists('div[class="wtrDown wtrLeft wtrStatusReadingNow"]', 'Set Book to currently-reading');
     });
   });
-
+  // take a screenshot to show the book has been set to currently reading
   casper.then(function(){
     this.wait(3000, function(){
       console.log("Make a screenshot and save it as AfterReadingSelected.png");
       this.capture('AfterReadingSelected.png');
     });
   });
-
+  // simulate a click navigating to the home page
   casper.then(function(){
     this.evaluate(function(){
       document.querySelector('a[href="/"]').click();
     });
   });
-
+  // test to see currently on home page
+  // loop through all the books that are currently reading checking against search term
+  // simulate a click to open up the module
   casper.then(function(){
     test.assertExists('body[class="gr-homePageBody"]', 'On home page');
     this.evaluate(function(){
@@ -86,7 +92,8 @@ casper.test.begin('test login for good reads and user experience', 4, function s
       }
     });
   });
-
+  // simulate a click to change from pages to percent in module
+  // set input to 27
   casper.then(function(){
     this.evaluate(function(){
       document.querySelector('button[class="buttonToggle"]').click();
@@ -95,20 +102,21 @@ casper.test.begin('test login for good reads and user experience', 4, function s
     // so I found this sendKeys method and it worked.
     this.sendKeys('input[class="gr-textInput updateReadingProgress__headerInput"]', '27');
   });
-
+  // simulate click to update changes in module
   casper.then(function(){
     this.evaluate(function(){
       document.querySelector('button[class="gr-button longTextPopupForm__submitButton"]').click();
     });
   });
-
+  //take a screen shot to show the books reading percent has been changed
   casper.then(function(){
     this.wait(3000, function(){
       console.log("Make a screenshot and save it as AfterUpdated.png");
       this.capture('AfterUpdated.png');
     });
   });
-
+  // query the dom and print out in console title author and percent to show
+  // everything worked
   casper.thenEvaluate(function(){
     title = document.querySelector('a[class="gr-book__titleLink gr-hyperlink gr-hyperlink--naked"]').innerHTML;
     author = document.querySelector('a[href="https://www.goodreads.com/author/show/2546.Chuck_Palahniuk"]').innerHTML;
@@ -117,7 +125,7 @@ casper.test.begin('test login for good reads and user experience', 4, function s
     console.log("Author: ", author);
     console.log("Percent complete: ", percent);
   });
-
+  // finish test
   casper.run(function(){
     test.done();
   });
---
+++ readme.md
@@ -0,0 +1,10 @@
+#Instalation
+  npm install -g phantomjs casperjs
+
+##To Run
+  casperjs test goodReadsAssert.js
+
+###Results
+  This should run through a basic user test for "goodReads.com", search for
+  the book fight club set it to currently reading and update the read progress
+  to 27%. As well as take 4 screen shots of the user walk through.
